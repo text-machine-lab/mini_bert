de33db9a01e6831410cf3208f1f878d60d1e8357
Shree Thatte <shree.thatte@gmail.com>
2022-11-18 17:03:57 -0500

    Added train test split

--- data_processing.py
+++ data_processing.py
@@ -8,4 +8,5 @@ with open('processed_data.json', 'r') as f:
 df = pandas.DataFrame.from_dict(data, orient="index")
 
 formatted_data=Dataset.from_pandas(df)
+formatted_data = formatted_data.train_test_split()
 formatted_data.save_to_disk("formatted_data")
