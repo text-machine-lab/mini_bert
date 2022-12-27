94a991a9bef76cdecf876450b72ae5177facba9d
danpechi <danpechi@gmail.com>
2022-12-26 21:25:34 -0600

    debugging filtering function

--- utils.py
+++ utils.py
@@ -31,24 +31,24 @@ def filter_example(task_name, example, vocab_set, contractions, additional_exclu
         sentence = example['inputs']
     elif 'sentence1' in features:
         sentence = example['sentence1'] + " " + example['sentence2']
-    elif subset in ['wsc', 'wsc.fixed', 'winogrande-xl']:
+    elif task_name in ['wsc', 'wsc.fixed', 'winogrande-xl']:
         t1 = list(features)[0]
         sentence = example[t1]
-    elif subset in ['conll2003', 'race', 'ai2_arc', 'martn-nguyen/adversarial_nli']:
+    elif task_name in ['conll2003', 'race', 'ai2_arc', 'martn-nguyen/adversarial_nli']:
         t1 = list(features)[1]
         sentence = example[t1]
-    elif subset in ['squad']:
+    elif task_name in ['squad']:
         t1 = list(features)[2]
         t2 = list(features)[3]
         sentence = example[t1] + " " + example[t2]
-    elif subset == "swag":
-        t1 = list(dataset[segment].features)[2]
-        t2 = list(dataset[segment].features)[6]
-        t3 = list(dataset[segment].features)[7]
-        t4 = list(dataset[segment].features)[8]
-        t5 = list(dataset[segment].features)[9]
+    elif task_name == "swag":
+        t1 = list(features)[2]
+        t2 = list(features)[6]
+        t3 = list(features)[7]
+        t4 = list(features)[8]
+        t5 = list(features)[9]
         sentence = example[t1] + " " + example[t2] + " " + example[t3] + " " + example[t4] + " " + example[t5]
-    elif subset in ['copa', 'piqa']:
+    elif task_name in ['copa', 'piqa']:
         t1 = list(features)[0]
         t2 = list(features)[1]
         t3 = list(features)[2]
@@ -92,6 +92,7 @@ def filter_dataset(core_dataset,
             use_auth_token=True if use_auth_token else None,
         )
     else:
+        task_name = core_dataset
         datasets = load_dataset(
             core_dataset,
             cache_dir=cache_dir,
