5124b426b5888090121449222ffbe4a73e9f1e5a
danpechi <danpechi@gmail.com>
2023-01-18 20:25:11 -0500

    whoops

--- run_glue_CustomConfigModel.py
+++ run_glue_CustomConfigModel.py
@@ -434,8 +434,12 @@ def main():
     
     except:
         print(f"\nCould not load the default version of the RoBERTa model. Trying to load the CustomConfig now...")
+        if model_args.model_name_or_path.split('/')[-1] != 'best_model':
+            rn = model_args.model_name_or_path.split('/')[-3]
+        else:
+            rn = model_args.model_name_or_path.split('/')[-2]
         config, model = load_model_for_finetuning(
-            run_name=model_args.model_name_or_path.split('/')[-1],
+            run_name=rn,
             config_name=model_args.config_name if model_args.config_name else model_args.model_name_or_path,
             num_labels=num_labels,
             finetuning_task=data_args.task_name,
