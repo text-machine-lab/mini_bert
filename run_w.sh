c91a55b41f058732678a18655d646866dd5c4d3b
danpechi <danpechi@gmail.com>
2023-01-18 20:27:26 -0500

    same

--- run_w.sh
+++ run_w.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
-declare -a datasets=("cola" "sst2" "mrpc" "qqp" "stsb" "mnli" "qnli" "rte" "wnli")
-
 declare -a dataset=("stsb")
 
 declare -a models=("good-voice-21" "devout-deluge-19" "young-glitter-16" "major-smoke-6")
@@ -26,7 +22,7 @@ do
                                         d="checkpoint_at_16000"
                                 fi
                                 echo "$i $j $k $h"
-                                python3 run_glue_CustomConfigModel.py --model_name=output_dir/$j/other_checkpoints/$d --filter_glue=$h --tokenizer=./tokenizer_selection_scripts/Tokenizer_files/roberta-base_19000 --task_name="$i" --do_train --do_eval --max_seq_length=128 --per_gpu_train_batch_size=32 --learning_rate=2e-4 --weight_decay=0 --num_train_epochs=5 --output_dir="output_dir/$i-$j-vE-s$k-f$h-new" --report_to=wandb --overwrite_output_dir --eval_steps=100 --seed=$k
+                                python3 run_glue_CustomConfigModel.py --model_name=output_dir/$j/random_model --filter_glue=$h --tokenizer=./tokenizer_selection_scripts/Tokenizer_files/roberta-base_19000 --task_name="$i" --do_train --do_eval --max_seq_length=128 --per_gpu_train_batch_size=32 --learning_rate=2e-4 --weight_decay=0 --num_train_epochs=5 --output_dir="output_dir/$i-$j-vE-s$k-f$h-rand" --report_to=wandb --overwrite_output_dir --eval_steps=100 --seed=$k
                         done
                 done
         done
