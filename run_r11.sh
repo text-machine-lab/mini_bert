b15effe89c622fece17ab791dc9485b8e7cd7dd6
danpechi <danpechi@gmail.com>
2023-01-19 19:09:08 -0500

    new rand runs

---
+++ run_r11.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+declare -a datasets=("cola" "sst2" "mrpc" "qqp" "stsb" "mnli" "qnli" "rte" "wnli")
+
+declare -a modelx=("feasible-surf-14")
+
+for h in 1 0
+do
+	for i in "${datasets[@]}"
+	do
+		for j in "${modelx[@]}"
+		do
+			for k in 0 1 2
+			do
+				echo "$i $j $k $h $d"
+				python3 run_glue_CustomConfigModel.py --model_name=output_dir/$j/random_model --filter_glue=$h --tokenizer=./tokenizer_selection_scripts/Tokenizer_files/roberta-base_19000 --task_name="$i" --do_train --do_eval --max_seq_length=128 --per_gpu_train_batch_size=32 --learning_rate=2e-4 --weight_decay=0 --num_train_epochs=5 --output_dir="output_dir/$i-$j-vE-s$k-f$h-rand" --report_to=wandb --overwrite_output_dir --eval_steps=100 --seed=$k
+			done
+		done
+	done
+done
